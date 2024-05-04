6dfb0ecc74f00a6fee73a377b2fd655365ad3306
Shelton Simbi <shltnsimbi@gmail.com>
2024-05-04 15:39:52 +0200

    Created a Navbar

---
+++ components/Logo.tsx
@@ -0,0 +1,26 @@
+import Image from "next/image";
+import Link from "next/link";
+import logo from "../public/images/logo.png";
+//import localFont from 'next/font/local'
+//import {cn} from '@/lib/utils'
+
+/** 
+const headingFont = localFont({
+  src: "../public/font.woff2",
+});*/
+
+const Logo = () => {
+  return (
+    <Link href={"/"}>
+      <div className="hover:opacity-75 transition items-center gap-x-2 hidden md:flex">
+        <Image src={logo} alt="Logo" height={30} width={30} />
+        {/** <p className={(cn"text-lg text-neutral-700 pb-1", headingFont.className)}>Nssa Taskit</p>  */}
+        <p className="text-lg text-neutral-700 pb-1">
+          Nssa Task<span className="italic text-amber-700">it.</span>
+        </p>
+      </div>
+    </Link>
+  );
+};
+
+export default Logo;
