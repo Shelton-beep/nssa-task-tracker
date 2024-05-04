5ac44d7a6e7dce704e17ca4e44bf53d19e74fd6d
Shelton Simbi <shltnsimbi@gmail.com>
2024-05-04 18:35:03 +0200

    protected routes using middleware and created a dashboard organization

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,69 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
 
-export default clerkMiddleware();
+const isProtectedRoute = createRouteMatcher([
+  "/organization(.*)",
+  "/select-org(.*)",
+]);
+
+export default clerkMiddleware((auth, req) => {
+  if (!isProtectedRoute(req) && auth().userId) {
+    let path = "/select-org";
+
+    if (auth().orgId) {
+      path = `/organization/${auth().orgId}`;
+    }
+
+    const orgSelection = new URL(path, req.url);
+    return NextResponse.redirect(orgSelection);
+  }
+
+  const signInPath = process.env.NEXT_PUBLIC_SIGN_IN_URL || "/sign-in";
+  if (!auth().userId && isProtectedRoute(req)) {
+    return NextResponse.redirect(new URL(signInPath, req.url));
+  }
+
+  if (
+    auth().userId &&
+    !auth().orgId &&
+    req.nextUrl.pathname !== "/select-org"
+  ) {
+    const orgSelection = new URL("/select-org", req.url);
+    return NextResponse.redirect(orgSelection);
+  }
+
+  if (isProtectedRoute(req)) auth().protect();
+});
 
 export const config = {
-  matcher: ["/((?!.+.[w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 };
+/** 
+import { authMiddleware } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+
+export default authMiddleware({
+  publicRoutes: ["/"],
+  afterAuth(auth, req) {
+    if (auth.userId && auth.isPublicRoute) {
+      let path = "/select-org";
+
+      if (auth.orgId) {
+        path = `/organization/${auth.orgId}`;
+      }
+
+      const orgSelection = new URL(path, req.url);
+      return NextResponse.redirect(orgSelection);
+    }
+
+    if (!auth.userId && !auth.isPublicRoute) {
+      return redirectToSignIn({ returnBackUrl: req.url });
+    }
+
+    if (auth.userId && !auth.orgId && req.nextUrl.pathname !== "/select-org") {
+      const orgSelection = new URL("/select-org", req.url);
+      return NextResponse.redirect(orgSelection);
+    }
+  },
+});
+*/
