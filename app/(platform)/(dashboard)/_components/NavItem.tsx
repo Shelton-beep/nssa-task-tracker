c19c4a2f4ebdafafb6dd7a1b96d0ef6921143645
Shelton Simbi <shltnsimbi@gmail.com>
2024-05-06 12:55:37 +0200

    Created a skeleton for the sidebar

--- app/(platform)/(dashboard)/_components/NavItem.tsx
+++ app/(platform)/(dashboard)/_components/NavItem.tsx
@@ -6,6 +6,7 @@ import {
   AccordionTrigger,
 } from "@/components/ui/accordion";
 import { Button } from "@/components/ui/button";
+import { Skeleton } from "@/components/ui/skeleton";
 import { cn } from "@/lib/utils";
 import { Activity, CreditCard, Layout, Settings } from "lucide-react";
 import Image from "next/image";
@@ -103,3 +104,14 @@ const NavItem = ({
 };
 
 export default NavItem;
+
+NavItem.Skeleton = function SkeletonNavItem() {
+  return (
+    <div className="flex items-center gap-x-2">
+      <div className="w-10 h-10 relative shrink-0">
+        <Skeleton className="h-full w-full absolute" />
+      </div>
+      <Skeleton className="h-10 w-full" />
+    </div>
+  );
+};
