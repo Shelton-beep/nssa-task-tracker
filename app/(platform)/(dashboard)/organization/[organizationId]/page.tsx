3644c9b5c0a68fb65ee960a2aa79be9630f2b0b7
Shelton Simbi <shltnsimbi@gmail.com>
2024-05-07 09:29:26 +0200

    Created a generic server action

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,5 +1,20 @@
+import { Button } from "@/components/ui/button";
+
 const OrganizationIdPage = () => {
-  return <div>OrganizationIdPage</div>;
+  return (
+    <div>
+      <form action={"/"}>
+        <input
+          id="title"
+          name="title"
+          required
+          placeholder="Enter a board title"
+          className="border-black border p-1"
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </div>
+  );
 };
 
 export default OrganizationIdPage;
---
+++ lib/create-safe-action.ts
@@ -0,0 +1,11 @@
+import { z } from "zod";
+
+export type FieldErrors<T> = {
+  [K in keyof T]?: string[];
+};
+
+export type ActionState<TInput, TOutput> = {
+  fieldErrors?: FieldErrors<TInput>;
+  error?: string | null;
+  data?: TOutput;
+};
