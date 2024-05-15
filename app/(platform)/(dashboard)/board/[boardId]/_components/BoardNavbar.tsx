37fb50a32fb58b8aac1dcbf457943c241363812e
Shelton Simbi <shltnsimbi@gmail.com>
2024-05-15 17:11:57 +0200

    created board options component

--- app/(platform)/(dashboard)/board/[boardId]/_components/BoardNavbar.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/BoardNavbar.tsx
@@ -1,17 +1,18 @@
-import { auth } from "@clerk/nextjs/server";
 import { Board } from "@prisma/client";
 import BoardTitleForm from "./board-title-form";
+import { BoardOptions } from "./board-options";
 
 interface BoardNavbarProps {
   data: Board;
 }
 
 const BoardNavbar = async ({ data }: BoardNavbarProps) => {
-  const { orgId } = auth();
-
   return (
     <div className="w-full h-14 z-[40] bg-black/50 fixed top-14 flex items-center px-6 gap-x-4 text-white">
       <BoardTitleForm data={data} />
+      <div className="ml-auto">
+        <BoardOptions id={data.id} />
+      </div>
     </div>
   );
 };
