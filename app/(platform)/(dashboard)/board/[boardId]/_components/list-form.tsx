89e5347cf38de533c452bb7fb2603ed960a4a767
Shelton Simbi <shltnsimbi@gmail.com>
2024-05-16 13:03:55 +0200

    Created  a list container with its components

---
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-form.tsx
@@ -0,0 +1,77 @@
+"use client";
+
+import { Plus, X } from "lucide-react";
+import { ListWrapper } from "./list-wrapper";
+import { ElementRef, useRef, useState } from "react";
+import { useEventListener, useOnClickOutside } from "usehooks-ts";
+import { FormInput } from "@/components/form/form-input";
+import { Input } from "@/components/ui/input";
+import { useParams } from "next/navigation";
+import FormSubmit from "@/components/form/form-submit";
+import { Button } from "@/components/ui/button";
+
+export const ListForm = () => {
+  const params = useParams();
+
+  const [isEditing, setIsEditing] = useState(false);
+  const formRef = useRef<ElementRef<"form">>(null);
+  const inputRef = useRef<ElementRef<"input">>(null);
+
+  const enableEditing = () => {
+    setIsEditing(true);
+    setTimeout(() => {
+      inputRef.current?.focus();
+    });
+  };
+
+  const disableEditing = () => {
+    setIsEditing(false);
+  };
+
+  const onKeyDown = (e: KeyboardEvent) => {
+    if (e.key === "Escape") {
+      disableEditing();
+    }
+  };
+
+  useEventListener("keydown", onKeyDown);
+  useOnClickOutside(formRef, disableEditing);
+
+  if (isEditing) {
+    return (
+      <ListWrapper>
+        <form
+          action=""
+          ref={formRef}
+          className="w-full p-3 rounded-md bg-white space-y-4 shadow-md"
+        >
+          <FormInput
+            id="title"
+            ref={inputRef}
+            className="text-sm px-2 py-1 h-7 font-medium border-transparent hover:border-input focus:border-input transition"
+            placeholder="Enter list title..."
+          />
+          <input hidden value={params.boardId} name="boardId" />
+          <div className="flex items-center gap-x-1">
+            <FormSubmit>Add list</FormSubmit>
+            <Button onClick={disableEditing} size={"sm"} variant={"ghost"}>
+              <X className="h-5 w-5" />
+            </Button>
+          </div>
+        </form>
+      </ListWrapper>
+    );
+  }
+
+  return (
+    <ListWrapper>
+      <button
+        onClick={enableEditing}
+        className="w-full rounded-md bg-white/80 hover:bg-white/50 transition p-3 flex items-center font-medium text-sm"
+      >
+        <Plus className="h-4 w-4 mr-2" />
+        Add a list
+      </button>
+    </ListWrapper>
+  );
+};
--- app/(platform)/(dashboard)/board/[boardId]/page.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/page.tsx
@@ -1,5 +1,30 @@
-const BoardIdPage = () => {
-  return <div>BoardIdPage</div>;
+import prisma from "@/lib/prisma-db";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+import { ListContainer } from "./_components/list-container";
+
+interface BoardIdPageProps {
+  params: { boardId: string };
+}
+
+const BoardIdPage = async ({ params }: BoardIdPageProps) => {
+  const { orgId } = auth();
+
+  if (!orgId) {
+    redirect("select-org");
+  }
+
+  const lists = await prisma.list.findMany({
+    where: { boardId: params.boardId, board: { orgId } },
+    include: { cards: { orderBy: { order: "asc" } } },
+    orderBy: { order: "asc" },
+  });
+
+  return (
+    <div className="p-4 h-full overflow-x-auto">
+      <ListContainer boardId={params.boardId} data={lists} />
+    </div>
+  );
 };
 
 export default BoardIdPage;
